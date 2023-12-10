29e9c4f32e1f042da30eadf3aabbb4b86f979262
ahmed-hasan92 <ahmed201130030@gmail.com>
2023-12-10 21:05:40 +0300

    f

--- wishlistr/App.js
+++ wishlistr/App.js
@@ -3,18 +3,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import MainNav from "./src/navigations/MainNav";
-<<<<<<< HEAD
 import { useEffect, useState } from "react";
 import AuthNav from "./src/navigations/AuthNav";
 import { checkToken, getToken } from "./src/apis/store";
 import UserContext from "./src/context/UserContext";
 import { PaperProvider } from "react-native-paper";
 import Toast, { ToastProvider } from "react-native-toast-message";
-=======
-import { useState } from "react";
-import WishlistNav from "./src/navigations/WishlistNav";
-import ProfileNav from "./src/navigations/ProfileNav";
->>>>>>> nora-wishlist
 
 export default function App() {
   const [user, setUser] = useState(false);
@@ -27,7 +21,6 @@ export default function App() {
 
   return (
     <QueryClientProvider client={new QueryClient()}>
-<<<<<<< HEAD
       <UserContext.Provider value={{ user, setUser }}>
         <PaperProvider>
           <NavigationContainer>
@@ -36,13 +29,6 @@ export default function App() {
           <Toast />
         </PaperProvider>
       </UserContext.Provider>
-=======
-      <NavigationContainer>
-        <MainNav />
-      </NavigationContainer>
-
-      {/* <Text>Hello</Text> */}
->>>>>>> nora-wishlist
     </QueryClientProvider>
   );
 }
