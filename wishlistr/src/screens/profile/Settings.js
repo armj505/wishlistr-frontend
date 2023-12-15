65c4fc42077f9de2e5f79b912a74af8e30df909e
ahmed-hasan92 <ahmed201130030@gmail.com>
2023-12-15 23:56:33 +0300

    intreceptor

--- wishlistr/src/screens/profile/Settings.js
+++ wishlistr/src/screens/profile/Settings.js
@@ -7,12 +7,8 @@ import { TextInput } from "react-native-paper";
 import { Button } from "react-native-paper";
 import { Text } from "react-native-paper";
 import * as Animatable from "react-native-animatable";
-import settingsPic from "../../../assets/settings-128.png";
-import { TextArea } from "react-native-ui-lib";
 import arrowPic from "../../../assets/arrow.png";
-import defaultPic from "../../../assets/default.png";
-import plus from "../../../assets/plus.png";
-import ReadOnlyTextInput from "../../components/readOnly/ReadOnlyTextInput";
+
 import { useNavigation } from "@react-navigation/core";
 import ROUTES from "../../navigations";
 
@@ -30,7 +26,6 @@ const Settings = () => {
       <View
         style={{
           flex: 1,
-          justifyContent: "space-between",
 
           alignItems: "center",
           flexDirection: "row",
@@ -43,13 +38,15 @@ const Settings = () => {
           <Image source={arrowPic} style={{ width: 25, height: 25 }} />
         </TouchableOpacity>
         <Text
-          style={{ color: theme.colors.surface, fontSize: 18, marginTop: 32 }}
+          style={{
+            color: theme.colors.surface,
+            fontSize: 18,
+            marginTop: 32,
+            marginLeft: 122,
+          }}
         >
-          Settings
+          Profile Settings
         </Text>
-        <TouchableOpacity style={{ right: 10, marginTop: 32 }}>
-          <Text style={{ color: theme.colors.surface }}>Save</Text>
-        </TouchableOpacity>
       </View>
       <View
         style={{
@@ -59,163 +56,77 @@ const Settings = () => {
           borderTopRightRadius: 10,
         }}
       >
-        <Image
-          style={{
-            alignSelf: "center",
-            marginTop: 10,
-            width: 100,
-            height: 100,
-            borderRadius: 50,
-            borderWidth: 1,
-            borderColor: "black",
-          }}
-          source={defaultPic}
-        />
-        <TouchableOpacity
-          onPress={() => console.log("pressed")}
-          style={{
-            position: "absolute",
-            alignSelf: "center",
-            marginTop: 90,
-
-            borderRadius: 50,
-          }}
-        >
-          <Image
-            source={plus}
-            style={{
-              width: 25,
-              height: 25,
-              borderRadius: 50,
-
-              borderWidth: 2,
-              borderColor: "black",
-
-              backgroundColor: "gray",
-            }}
-          />
-        </TouchableOpacity>
         <View
           style={{
             width: "100%",
-            marginTop: 24,
-
-            height: 60,
-
-            flexDirection: "row",
-            justifyContent: "space-evenly",
-            alignItems: "center",
-          }}
-        >
-          <TouchableOpacity onPress={handleTextInputValue}>
-            <ReadOnlyTextInput
-              label="First Name"
-              value="Ahmed"
-              h={35}
-              w={160}
-              editable={isEditable}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={handleTextInputValue}>
-            <ReadOnlyTextInput
-              label="Last Name"
-              value="Hasan"
-              h={35}
-              w={160}
-              editable={isEditable}
-            />
-          </TouchableOpacity>
-        </View>
-        <View
-          style={{
-            width: "100%",
-            height: 60,
-            alignItems: "center",
-            marginTop: 5,
-          }}
-        >
-          <TouchableOpacity onPress={handleTextInputValue}>
-            <ReadOnlyTextInput
-              label="Gender"
-              value="male"
-              h={35}
-              w={340}
-              editable={isEditable}
-            />
-          </TouchableOpacity>
-        </View>
-        <View
-          style={{
-            width: "100%",
-            height: 60,
-            alignItems: "center",
-            marginTop: 5,
-          }}
-        >
-          <TouchableOpacity onPress={handleTextInputValue}>
-            <ReadOnlyTextInput
-              label="Email address"
-              value="ahmed@wishlistr.com"
-              h={35}
-              w={340}
-            />
-          </TouchableOpacity>
-        </View>
-        <View
-          style={{
-            width: "100%",
-            height: 60,
-            alignItems: "center",
-            marginTop: 5,
-          }}
-        >
-          <TouchableOpacity onPress={handleTextInputValue}>
-            <ReadOnlyTextInput label="Address" value="Kuwait" h={35} w={340} />
-          </TouchableOpacity>
-        </View>
-        <View
-          style={{
-            width: "100%",
-            height: 60,
-            alignItems: "center",
-            marginTop: 5,
-          }}
-        >
-          <TouchableOpacity onPress={handleTextInputValue}>
-            <ReadOnlyTextInput
-              label="Phone Number"
-              value="123"
-              h={35}
-              w={340}
-            />
-          </TouchableOpacity>
-        </View>
-        <View
-          style={{
-            width: "100%",
-            backgroundColor: "gray",
-            borderBottomWidth: 1,
-            borderTopWidth: 1,
-          }}
-        >
-          <Text style={{ color: "white", paddingLeft: 10 }}>
-            Account Settings
-          </Text>
-        </View>
-        <TouchableOpacity
-          onPress={() => {
-            deleteToken();
-            setUser(false);
-          }}
-          style={{
-            backgroundColor: "black",
+            backgroundColor: "pink",
+            height: 250,
             marginTop: 20,
-            paddingVertical: 5,
-            width: 70,
+            alignItems: "center",
           }}
         >
-          <Text style={{ color: theme.colors.surface }}>SignOut</Text>
-        </TouchableOpacity>
+          <View
+            style={{
+              width: "100%",
+              backgroundColor: "gray",
+              borderBottomWidth: 1,
+            }}
+          >
+            <Text style={{ color: "black" }}>Change Password</Text>
+          </View>
+          <TextInput
+            secureTextEntry
+            mode="outlined"
+            style={{
+              width: 350,
+              marginTop: 10,
+              marginLeft: 10,
+              fontSize: 15,
+
+              borderColor: "black",
+            }}
+            label="Current Password"
+          />
+          <TextInput
+            secureTextEntry
+            mode="outlined"
+            style={{
+              width: 350,
+              marginTop: 10,
+              marginLeft: 10,
+              fontSize: 15,
+
+              borderColor: "black",
+            }}
+            label="New Password"
+          />
+          <TouchableOpacity
+            style={{
+              alignSelf: "flex-end",
+              marginTop: 10,
+              width: 100,
+              backgroundColor: "gray",
+              padding: 8,
+              marginRight: 15,
+              borderRadius: 10,
+              elevation: 50,
+            }}
+          >
+            <Text
+              style={{
+                alignSelf: "center",
+                color: "white",
+                fontSize: 16,
+                fontWeight: "bold",
+              }}
+            >
+              Save
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{ width: "100%", backgroundColor: "green", height: 200 }}
+        ></View>
       </View>
     </View>
   );
@@ -224,127 +135,3 @@ const Settings = () => {
 export default Settings;
 
 const styles = StyleSheet.create({});
-
-// <View style={{ flex: 1, justifyContent: "flex-end" }}>
-//   <View
-//     style={{
-//       flex: 1,
-
-//       justifyContent: "center",
-//       alignItems: "center",
-//     }}
-//   >
-//     <Text style={{ color: "white", fontSize: 16, fontWeight: "bold" }}>
-//       Settings
-//     </Text>
-//   </View>
-//   <View
-//     style={{
-//       flex: 9,
-//       backgroundColor: theme.colors.surface,
-//       borderTopLeftRadius: 10,
-//       borderTopRightRadius: 10,
-//     }}
-//   >
-//     <View
-//       style={{
-//         width: "100%",
-//         backgroundColor: "yellow",
-//         borderBottomWidth: 1,
-//       }}
-//     >
-//       <Text style={{ marginTop: 10 }}>Profile settings</Text>
-//     </View>
-//     <View style={{ width: "100%", height: 400 }}>
-//       <View
-//         style={{
-//           width: "100%",
-//           height: 50,
-//           // backgroundColor: "red",
-//           borderBottomWidth: 1,
-//           flexDirection: "row",
-//           justifyContent: "flex-end",
-//         }}
-//       >
-//         <Text style={{ alignSelf: "flex-start" }}>First Name</Text>
-//       </View>
-//       <View
-//         style={{
-//           width: "100%",
-//           height: 50,
-//           // backgroundColor: "red",
-//           borderBottomWidth: 1,
-//         }}
-//       ></View>
-//       <View
-//         style={{
-//           width: "100%",
-//           height: 50,
-//           // backgroundColor: "red",
-//           borderBottomWidth: 1,
-//         }}
-//       ></View>
-//       <View
-//         style={{
-//           width: "100%",
-//           height: 50,
-//           // backgroundColor: "red",
-//           borderBottomWidth: 1,
-//         }}
-//       ></View>
-//       <View
-//         style={{
-//           width: "100%",
-//           height: 50,
-//           // backgroundColor: "red",
-//           borderBottomWidth: 1,
-//         }}
-//       ></View>
-//       <View
-//         style={{
-//           width: "100%",
-//           height: 50,
-//           // backgroundColor: "red",
-//           borderBottomWidth: 1,
-//         }}
-//       ></View>
-//       <View
-//         style={{
-//           width: "100%",
-//           height: 50,
-//           // backgroundColor: "red",
-//           borderBottomWidth: 1,
-//         }}
-//       ></View>
-//       <View
-//         style={{
-//           width: "100%",
-//           height: 50,
-//           // backgroundColor: "red",
-//           borderBottomWidth: 1,
-//         }}
-//       ></View>
-//     </View>
-//     <View style={{ width: "100", height: 50, backgroundColor: "gray" }}>
-//       <Text>App Settings</Text>
-//     </View>
-//     <View style={{ width: "100%", backgroundColor: "green", height: 160 }}>
-//       <View
-//         style={{
-//           width: "100%",
-//           height: 60,
-//           backgroundColor: "red",
-//           borderBottomWidth: 1,
-//         }}
-//       ></View>
-//       <View
-//         style={{
-//           width: "100%",
-//           height: 60,
-//           backgroundColor: "red",
-//           borderBottomWidth: 1,
-//         }}
-//       ></View>
-//     </View>
-//   </View>
-// </View>
