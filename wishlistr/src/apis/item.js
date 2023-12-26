5d2c0e2418e400b382d1cf068581b0de63c8b141
ahmed-hasan92 <ahmed201130030@gmail.com>
2023-12-26 19:26:43 +0300

    done

--- wishlistr/src/apis/item.js
+++ wishlistr/src/apis/item.js
@@ -9,5 +9,13 @@ const getItem = async (itemId) => {
   const { data } = await instance.get(`/item/${itemId}`);
   return data;
 };
+const addItemToList = async (wishListId, itemId) => {
+  const response = await instance.put(`/wishlist/${wishListId}/${itemId}`);
+  return response.data;
+};
 
-export { getAllItems, getItem };
+const removeItemFromList = async (itemId, wishListId) => {
+  const response = await instance.put(`/item/${itemId}/${wishListId}`);
+  return response.data;
+};
+export { getAllItems, getItem, addItemToList, removeItemFromList };
--- wishlistr/src/screens/wishList/ItemDetails.js
+++ wishlistr/src/screens/wishList/ItemDetails.js
@@ -1,12 +1,52 @@
-import { StyleSheet, Text, View, Image } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import Screen from "../../components/ui/Screen";
 import Title from "../../components/ui/Title";
 import { ScrollView } from "react-native-gesture-handler";
 import PrimaryButton from "../../components/ui/PrimaryButton";
+import Modal from "react-native-modal";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getMyProfile } from "../../apis/profile";
+import { getAllWishlists } from "../../apis/wishList";
+import { addItemToList } from "../../apis/item";
+import Toast from "react-native-toast-message";
+import { useNavigation } from "@react-navigation/native";
+import ROUTES from "../../navigations";
 
 const ItemDetails = ({ route }) => {
+  const query = useQueryClient();
+  const navigation = useNavigation();
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [selectedWishlist, setSelectedWishlist] = useState(null);
+  const [selectedWishlistId, setSelectedWishlistId] = useState(null);
+  const { data: myLists, isLoading } = useQuery({
+    queryKey: ["myLists"],
+    queryFn: () => getAllWishlists(),
+  });
+  const { mutate: addToList } = useMutation({
+    mutationKey: ["addToList"],
+    mutationFn: () => addItemToList(selectedWishlist._id, item._id),
+    onSuccess: () => {
+      Toast.show({
+        type: "success",
+        text1: "The Item has been added",
+        visibilityTime: 2000,
+      });
+      navigation.navigate(ROUTES.HOME.HOME.HOME);
+      query.invalidateQueries("wishlists");
+    },
+  });
+
+  if (isLoading) {
+    return (
+      <View>
+        <Text>Fetching wishLists data</Text>
+      </View>
+    );
+  }
+
   const { item } = route.params;
+
   return (
     <Screen>
       <Title title={item?.name} />
@@ -22,7 +62,7 @@ const ItemDetails = ({ route }) => {
           <Text style={styles.itemPrice}>{item?.price} KD</Text>
           <PrimaryButton
             name="Add to Wishlist"
-            press={() => console.log("button is pressed")}
+            press={() => setModalVisible(true)}
           />
           <View style={styles.itemDescriptionContainer}>
             <Text style={styles.productDetailsTitle}>Product Details</Text>
@@ -30,6 +70,53 @@ const ItemDetails = ({ route }) => {
           </View>
         </View>
       </ScrollView>
+      <Modal isVisible={isModalVisible} style={styles.modal}>
+        <View style={styles.modalContent}>
+          <Text style={styles.modalTitle}>Choose a Wishlist</Text>
+          {myLists?.map((oneList) => (
+            <TouchableOpacity
+              key={oneList._id}
+              onPress={() => {
+                setSelectedWishlist(oneList);
+                setSelectedWishlistId(oneList._id);
+              }}
+              style={[
+                styles.listItem,
+                selectedWishlistId === oneList._id && styles.selectedListItem,
+              ]}
+            >
+              <Text
+                style={[
+                  styles.listItemText,
+                  selectedWishlistId === oneList._id &&
+                    styles.selectedListItemText,
+                ]}
+              >
+                {oneList.name}
+              </Text>
+            </TouchableOpacity>
+          ))}
+          <View style={styles.modalButtons}>
+            <TouchableOpacity
+              onPress={() => {
+                if (selectedWishlist) {
+                  addToList();
+                }
+                setModalVisible(false);
+              }}
+              style={styles.addButton}
+            >
+              <Text style={styles.buttonText}>Add</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => setModalVisible(false)}
+              style={styles.cancelButton}
+            >
+              <Text style={styles.buttonText}>Cancel</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </Screen>
   );
 };
@@ -53,7 +140,6 @@ const styles = StyleSheet.create({
   imageContainer: {
     width: "100%",
     height: 360,
-    // borderWidth: 1,
     borderRadius: 16,
     overflow: "hidden",
   },
@@ -65,11 +151,9 @@ const styles = StyleSheet.create({
   brandText: {
     paddingTop: 8,
     fontSize: 16,
-    // fontWeight: "bold",
     color: "#888",
   },
   itemTitle: {
-    // paddingTop: 0,
     fontSize: 24,
     fontWeight: "bold",
   },
@@ -89,5 +173,59 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "bold",
     paddingBottom: 16,
+  },
+  modal: {
+    justifyContent: "flex-end",
+    margin: 0,
+  },
+  modalContent: {
+    backgroundColor: "white",
+    padding: 16,
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+    height: 250,
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  listItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#eee",
+  },
+  listItemText: {
+    fontSize: 16,
+  },
+  selectedListItem: {
+    backgroundColor: "#eee",
+  },
+  selectedListItemText: {
+    color: "blue",
+  },
+  modalButtons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 16,
+  },
+  addButton: {
+    backgroundColor: "green",
+    padding: 10,
+    borderRadius: 8,
+    flex: 1,
+    marginRight: 8,
+  },
+  cancelButton: {
+    backgroundColor: "red",
+    padding: 10,
+    borderRadius: 8,
+    flex: 1,
+    marginLeft: 8,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 16,
   },
 });
